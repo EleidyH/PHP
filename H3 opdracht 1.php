660239249b7befb78b5f74c65b5cd606b5ac8667
EleidyH <eleidyhoyer@hotmail.com>
2023-11-15 10:12:49 +0100

    naam

--- H3 opdracht 1.php
+++ H3 opdracht 1.php
@@ -2,17 +2,43 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>H3 opdracht1</title>
+        <title>Hello World Example</title>
     </head>
     <body>
         <?php
+        //  Een voorbeeld van een echo en een print
+        echo "<h1>Hallo wereld,</h1>";
+        echo "Ik zit op het ";
+
+        // Voorbeeld van een variabele in PHP.
+        $schoolnaam = "Techniek College Rotterdam!";
+
+        // Met echo kun je iets op het scherm tonen.
+        echo "$schoolnaam";
+        
+        //Mijn naam in variabele.
+        $name = "Eleidy";
+        echo "<br><br>Hallo: $name";
+        echo "<br><br>";
+        
+        //H3 opdracht 1.
         date_default_timezone_set("Europe/Amsterdam");
+        $today = date("j F Y");
+        echo "Het is vandaag: $today";
         
-        $today = date("J F Y");
         echo "<br>";
+        $jaar = date("y");
+        echo "Vandaag is het de $jaar dag van het jaar";
         
+        echo"<br>";
+        $dag = date("l");
+        $dageninweek = date("w");
+        echo "$dag is de $dageninweek dag van de week";
         
-        
+        echo "<br>";
+        $maand = date("F");
+        $maanddagen = date("t");
+        echo "De maand $maand heeft in totaal $maanddagen dagen.";
         ?>
     </body>
 </html>
