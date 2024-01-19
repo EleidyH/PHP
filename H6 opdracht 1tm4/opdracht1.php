1639c10cb4b3922e73b01d1bfad8dde38e7fe271
EleidyH <144007961+EleidyH@users.noreply.github.com>
2024-01-19 10:17:41 +0100

    update

--- H6 opdracht 1tm4/opdracht1.php
+++ H6 opdracht 1tm4/opdracht1.php
@@ -1,5 +1,15 @@
 <?php
+// Start de sessie
+session_start();
 
+// Controleer of de sessievariabele is ingesteld, zo niet, initialiseer deze met 0
+if (!isset($_SESSION['page_visits'])) {
+    $_SESSION['page_visits'] = 0;
+}
 
+// Verhoog het aantal bezoeken bij elke paginaweergave
+$_SESSION['page_visits']++;
 
+// Toon het aantal bezoeken
+echo "Aantal keer bezocht: " . $_SESSION['page_visits'];
 ?>
